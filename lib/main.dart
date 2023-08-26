6f9d587132a1a85d437df94ff0dc2ab508a75eab
Ahmet Hakan DİNGER <hdngr@hotmail.com.tr>
2023-08-26 16:09:49 +0300

    08/26/2023-16:09

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -17,9 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Barkod Uygulaması',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: 'Roboto'),
       home: MyHomePage(),
     );
   }
@@ -144,8 +143,20 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: Text('ScanExcel'),
         actions: [
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () async {
+              final UrlPreview =
+                  'https://play.google.com/store/apps/details?id=com.bimilyondunya.eventswork&pcampaignid=web_share';
+
+              await Share.share(
+                  'Hey Senin İçin Bulduğum Uygulamaya Bir Göz At!\n\n$UrlPreview');
+            },
+          ),
+         
           PopupMenuButton(
             offset: Offset(0, 50),
             padding: EdgeInsets.zero,
@@ -159,17 +170,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 borderRadius: BorderRadius.all(Radius.circular(10))),
             color: Colors.grey[200],
           ),
-          PopupMenuItem(
-            value: 'item2',
-            child: Row(
-              children: [
-                Text('Item 2'),
-                SizedBox(
-                    width: 8), // add some spacing between the text and the icon
-                Icon(Icons.share), // add an icon to the right of the item
-              ],
-            ),
-          ),
         ],
       ),
       body: Padding(
@@ -177,17 +177,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ElevatedButton(
-              onPressed: scanBarcode,
-              child: Text('Barkod Tara'),
-            ),
             SizedBox(height: 16.0),
             TextField(
               controller: barcodeController,
               decoration: InputDecoration(
                 labelText: 'Taranan Barkod',
                 suffixIcon: IconButton(
-                  icon: Icon(Icons.camera),
+                  icon: Icon(Icons.camera_alt_sharp),
                   onPressed: scanBarcode,
                 ),
                 border: OutlineInputBorder(
@@ -199,37 +195,110 @@ class _MyHomePageState extends State<MyHomePage> {
             TextField(
               controller: manualInputController,
               decoration: InputDecoration(
-                labelText: 'Manuel Giriş',
+                labelText: 'Açıklama',
                 suffixIcon: IconButton(
-                  icon: Icon(Icons.drafts),
+                  icon: Icon(Icons.border_color_outlined),
                   onPressed: scanBarcode,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
               ),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: saveData,
-              child: Text('Veri Tabanına Kaydet'),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DataPage()),
-                );
-              },
-              child: Text('Verileri Görüntüle'),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: clearDatabase,
-              child: Text('Veri Tabanını Temizle'),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: exportToExcel,
-              child: Text('Excele Aktar'),
+            SizedBox(height: 20),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SquareButton(
+                  color: Color.fromARGB(255, 105, 216, 111),
+                  onPressed: saveData,
+                  buttonName: 'Veri Tabanına Kaydet',
+                  icon: Icon(Icons.all_inbox_outlined),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SquareButton(
+                  color: Color.fromARGB(255, 105, 216, 111),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DataPage()),
+                    );
+                  },
+                  buttonName: 'Verileri Görüntüle',
+                  icon: Icon(Icons.search_outlined),
+                ),
+              ),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SquareButton(
+                  color: Color.fromARGB(255, 105, 216, 111),
+                  onPressed: clearDatabase,
+                  buttonName: 'Veri Tabanını Temizle',
+                  icon: Icon(Icons.delete_forever),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SquareButton(
+                  color: Color.fromARGB(255, 105, 216, 111),
+                  onPressed: exportToExcel,
+                  buttonName: 'Excele Aktar',
+                  icon: Icon(Icons.upload_file_rounded),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SquareButton extends StatelessWidget {
+  final Color color;
+  final VoidCallback onPressed;
+  final String buttonName;
+  final Icon? icon;
+
+  SquareButton({
+    required this.color,
+    required this.onPressed,
+    required this.buttonName,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: 64,
+        maxHeight: 64,
+        minWidth: 128,
+        maxWidth: 128,
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: color,
+          side: BorderSide(
+            color: Colors.grey,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null) icon!,
+            Text(
+              buttonName,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
